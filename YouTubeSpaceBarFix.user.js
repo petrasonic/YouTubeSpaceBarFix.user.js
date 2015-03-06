30036d7baeade6d1ebb076563aa6bc2dc43c9c39
David Petrasovic <davidpetrasovic@gmail.com>
2015-03-06 10:38:02 -0500

    code cleanup

--- YouTubeSpaceBarFix.user.js
+++ YouTubeSpaceBarFix.user.js
@@ -1,23 +1,22 @@
 // ==UserScript==
 // @name         YouTube Spacebar & Fullscreen fix
 // @namespace    http://davidpetrasovic.com
-// @version      0.1
+// @version      0.2
 // @description  make space bar and "f" work for youtube without clicking on the video first.
 // @author       Dave
 // @match        https://www.youtube.com/*
 // @grant        none
-// @require http://code.jquery.com/jquery-latest.js
+// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 $(document).ready(function(){
     
     $(document).keypress(function(e) {
-        if(e.which == 32) {
-            // enter pressed
+        if(e.which == 32) {//spacebar
             e.preventDefault();
             console.log($("#player .html5-video-controls .ytp-button-pause").click());
             console.log($("#player .html5-video-controls .ytp-button-play").click());
-        }else if(e.which == 102){
+        }else if(e.which == 102){//F for fullscreen
             console.log($("#player .html5-video-controls .ytp-button-fullscreen-enter").click());
             console.log($("#player .html5-video-controls .ytp-button-fullscreen-exit").click());
         }
