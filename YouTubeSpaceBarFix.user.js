5aa40d7fccc6bbff7195fc5bf331faed0dec9dfb
David Petrasovic <davidpetrasovic@gmail.com>
2015-03-17 00:27:01 -0400

    Fix for spacebar while typing in search field or other text input.

--- YouTubeSpaceBarFix.user.js
+++ YouTubeSpaceBarFix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Spacebar & Fullscreen fix
 // @namespace    http://davidpetrasovic.com
-// @version      0.2
+// @version      0.3
 // @description  make space bar and "f" work for youtube without clicking on the video first.
 // @author       Dave
 // @match        https://www.youtube.com/*
@@ -12,13 +12,15 @@
 $(document).ready(function(){
     
     $(document).keypress(function(e) {
-        if(e.which == 32) {//spacebar
-            e.preventDefault();
-            console.log($("#player .html5-video-controls .ytp-button-pause").click());
-            console.log($("#player .html5-video-controls .ytp-button-play").click());
-        }else if(e.which == 102){//F for fullscreen
-            console.log($("#player .html5-video-controls .ytp-button-fullscreen-enter").click());
-            console.log($("#player .html5-video-controls .ytp-button-fullscreen-exit").click());
+        if(!$("input").is(":focus")){
+            if(e.which == 32) {//spacebar
+                e.preventDefault();
+                console.log($("#player .html5-video-controls .ytp-button-pause").click());
+                console.log($("#player .html5-video-controls .ytp-button-play").click());
+            }else if(e.which == 102){//F
+                console.log($("#player .html5-video-controls .ytp-button-fullscreen-enter").click());
+                console.log($("#player .html5-video-controls .ytp-button-fullscreen-exit").click());
+            }
         }
     });
 });
